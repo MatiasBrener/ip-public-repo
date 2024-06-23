12d339e3245494f5094c5995f296f47544293e63
MatiasBrener <matiasbrener@live.com.ar>
2024-06-23 17:33:01 -0300

    Primer commit IP - MB

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -1,20 +1,37 @@
+# Carpeta: nasa_image_gallery Archivo: layers/services/services_nasa_image_gallery.py
+
 # capa de servicio/lógica de negocio
 
-from ..transport import transport
+from nasa_image_gallery.layers.transport import transport
 from ..dao import repositories
-from ..generic import mapper
+from nasa_image_gallery.layers.generic import mapper
 from django.contrib.auth import get_user
+from nasa_image_gallery.models import Favourite
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+# obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
+# # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
 
 def getAllImages(input=None):
-    # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
-    # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
+   
     json_collection = []
-
     images = []
-
-    # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
-
+    if input:
+        json_collection = transport.getAllImages(input)
+    else:
+        json_collection = transport.getAllImages(None)
+    
+    for json in json_collection:
+        nasa_card = mapper.fromRequestIntoNASACard(json)
+        images.append(nasa_card)
+    
     return images
+
+    
+# recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
 
 
 def getImagesBySearchInputLike(input):
@@ -22,11 +39,11 @@ def getImagesBySearchInputLike(input):
 
 
 # añadir favoritos (usado desde el template 'home.html')
-def saveFavourite(request):
-    fav = '' # transformamos un request del template en una NASACard.
-    fav.user = '' # le seteamos el usuario correspondiente.
+def save_Favourite(request):
+    fav = mapper.fromTemplateIntoNASACard(request)# transformamos un request del template en una NASACard.
+    fav.user =request.user # le seteamos el usuario correspondiente.
+    return repositories.saveFavourite(fav)
 
-    return repositories.saveFavourite(fav) # lo guardamos en la base.
 
 
 # usados en el template 'favourites.html'
@@ -36,11 +53,11 @@ def getAllFavouritesByUser(request):
     else:
         user = get_user(request)
 
-        favourite_list = [] # buscamos desde el repositorio TODOS los favoritos del usuario (variable 'user').
+        favourite_list = repositories.getAllFavouritesByUser(user) # buscamos desde el repositorio TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
 
         for favourite in favourite_list:
-            nasa_card = '' # transformamos cada favorito en una NASACard, y lo almacenamos en nasa_card.
+            nasa_card = mapper.fromRequestIntoNASACard(favourite)# transformamos cada favorito en una NASACard, y lo almacenamos en nasa_card.
             mapped_favourites.append(nasa_card)
 
         return mapped_favourites
--- nasa_image_gallery/views.py
+++ nasa_image_gallery/views.py
@@ -1,10 +1,14 @@
+# Carpeta: nasa_image_gallery Archivo: views.py 
+
 # capa de vista/presentación
 # si se necesita algún dato (lista, valor, etc), esta capa SIEMPRE se comunica con services_nasa_image_gallery.py
 
 from django.shortcuts import redirect, render
+from nasa_image_gallery.models import Favourite
 from .layers.services import services_nasa_image_gallery
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib import messages
 
 # función que invoca al template del índice de la aplicación.
 def index_page(request):
@@ -14,7 +18,6 @@ def index_page(request):
 def getAllImagesAndFavouriteList(request):
     images = []
     favourite_list = []
-
     return images, favourite_list
 
 # función principal de la galería.
@@ -23,35 +26,61 @@ def home(request):
     # (*) este último, solo si se desarrolló el opcional de favoritos; caso contrario, será un listado vacío [].
     images = []
     favourite_list = []
+    images, favourite_list = getAllImagesAndFavouriteList(request)
+    images = services_nasa_image_gallery.getAllImages()   
     return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list} )
 
 
 # función utilizada en el buscador.
+
+
 def search(request):
     images, favourite_list = getAllImagesAndFavouriteList(request)
     search_msg = request.POST.get('query', '')
+    if not search_msg:
+        search_msg="space"
+    else:
+        images=services_nasa_image_gallery.getAllImages(search_msg) #
+    return render(request, 'home.html', {'images': images, 'favourite_list': favourite_list})
 
-    # si el usuario no ingresó texto alguno, debe refrescar la página; caso contrario, debe filtrar aquellas imágenes que posean el texto de búsqueda.
-    pass
 
 
 # las siguientes funciones se utilizan para implementar la sección de favoritos: traer los favoritos de un usuario, guardarlos, eliminarlos y desloguearse de la app.
 @login_required
 def getAllFavouritesByUser(request):
-    favourite_list = []
+    favourite_list = Favourite.objects.filter(user=request.user)
     return render(request, 'favourites.html', {'favourite_list': favourite_list})
 
 
 @login_required
 def saveFavourite(request):
-    pass
+     if request.method == 'POST':
+        try:
+            saved_favourite = services_nasa_image_gallery.save_Favourite(request)
+            if saved_favourite:
+                return redirect('favoritos')  # Redirige a la vista de favoritos si el favorito se guarda correctamente
+            else:
+                messages.error(request, 'Error al guardar el favorito.')
+        except Exception as e:
+            messages.error(request, f'Error: {str(e)}')
+    
+        return redirect('home')
 
 
 @login_required
 def deleteFavourite(request):
-    pass
+    if request.method == "POST":
+        success = services_nasa_image_gallery.deleteFavourite(request)
+        if success: 
+            return redirect('favoritos')  # Redirigir a la lista de favoritos después de la eliminación exitosa
+        else:
+            messages.error(request, 'Error al guardar el favorito.')
+            return redirect('favoritos')  # Aquí redirigimos de todos modos, puedes personalizar esto.
+    else:
+        return redirect('favoritos')
 
 
 @login_required
 def exit(request):
-    pass
+    logout(request)
+    return redirect('home.html')
